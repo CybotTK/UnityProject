fd9ea732e83736cb6e35124079a9c2c4c8b2109a
Cybot <stancilaionutmarian@yahoo.com>
2025-01-14 12:59:00 +0200

    Fixed Rendering Pipeline + Cell script

--- Assets/Scripts/TicTacToeScripts/Cell.cs
+++ Assets/Scripts/TicTacToeScripts/Cell.cs
@@ -7,6 +7,9 @@ using TMPro;
 public class Cell : MonoBehaviour
 {
     public TextMeshProUGUI mLabel;
+    public Image targetImage;
+    public Sprite redChickenSprite;
+    public Sprite cyanChickenSprite;
     public Button mButton;
     public TicTacToeMain mMain;
 
@@ -15,6 +18,24 @@ public class Cell : MonoBehaviour
         mButton.interactable = false;
 
         mLabel.text = mMain.GetTurnCharacter();
+        Color color = mLabel.color;
+        color.a = 0; // Fully transparent
+        mLabel.color = color;
+
+        if (mMain.GetTurnCharacter() == "X") {
+            targetImage.sprite = redChickenSprite;
+            color = targetImage.color;
+            color.a = 1 ;
+            targetImage.color = color;
+
+        }
+        else
+        {
+            targetImage.sprite= cyanChickenSprite;
+            color = targetImage.color;
+            color.a = 1;
+            targetImage.color = color;
+        }
 
         mMain.Switch();
     }
